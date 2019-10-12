a222108de712c76fc60b3950104d747e348072f5
Cesar <cesarlecona@gmail.com>
2019-10-12 11:01:25 -0500

    Subiendo un primer archivo de ruby

---
+++ Primer_Programa.rb
@@ -0,0 +1,22 @@
+
+print "Mi Primer Programa en Ruby"
+print " ------ Bienvenido a Ruby"
+# Este es un comentario de una línea
+puts "En la misma línea"
+puts "En otra linea"
+
+=begin
+Este es un comentario 
+de varias líneas
+que es útil para agreagar 
+la descripción del código
+=end
+
+print "Por favor ingresa tu nombre: "
+name= gets
+print "Por favor ingresa tu apellido: "
+last= gets
+print "Tu nombre es:" + name + " " + last
+
+
+ 
