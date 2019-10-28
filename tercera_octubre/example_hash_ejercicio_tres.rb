07051fb63758e5dc93c0aa2c494f8e7ec8f91580
Polo <nathan.rx@gmail.com>
2019-10-28 13:47:15 -0600

    Week 3

--- tercera_octubre/example_hash_ejercicio_tres.rb
+++ tercera_octubre/example_hash_ejercicio_tres.rb
@@ -3,18 +3,19 @@ def example_num_hash
 
   num_hash = {
 
-      num_0: 0,
       num_1: 1,
+      num_0: 0,
       num_2: 2,
       num_3: 3,
       num_4: 4,
-      num_5: 5,
       num_6: 6,
+      num_5: 5,
 
   }
 
-  puts num_hash.min
-  puts num_hash.max
+  num_hash.sort
+  puts num_hash.sort.min
+  puts num_hash.sort.max
 
 end
 
